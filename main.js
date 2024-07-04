9ec7d08059de45ac8f76ae46e404c9df9e4db0fe
spepeters <silenthgame@gmail.com>
2024-07-04 00:32:07 -0700

    asdf

--- main.js
+++ main.js
@@ -24,8 +24,6 @@ function pageSetup() {
     // Get the element by its ID
     var element = document.getElementById("bg");
 
-    var test = "hello"
-
     // Define the CSS background property
     var backgroundStyle = "linear-gradient( rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0.8) 100%), url(https://64.media.tumblr.com/2d2dcd2bcb9329fc65e5da7aa35ac1f2/5df7e79300dfd01b-3f/s1280x1920/6054d024cb443ed46d6f68e7effd509161a0b582.gifv)";
 
@@ -35,11 +33,3 @@ function pageSetup() {
 }
 
 pageSetup()
-
-
-
-
-
-
-
-
